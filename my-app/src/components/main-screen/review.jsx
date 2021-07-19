bcb0e60237e33819e0234f9fbdd3d813a72aa705
Revezer <64742445+Revezer@users.noreply.github.com>
2021-07-19 15:55:47 +0300

    отзывы

--- my-app/src/components/main-screen/review.jsx
+++ my-app/src/components/main-screen/review.jsx
@@ -6,23 +6,23 @@ const Review = (props) => {
     return (
         <div className='review__conteiner'>
             <span className='review__conteiner-name'>{review.name}</span>
-            <div className='review__conteiner-dignity'>
-                <span>Достоинства</span>
-                <span>{review.dignity}</span>
+            <div className='review__conteiner-dignity dignity'>
+                <span className='dignity-title'>Достоинства</span>
+                <span className='dignity-text'>{review.dignity}</span>
             </div>
-            <div className='review__conteiner-limitation'>
-                <span>Недостатки</span>
-                <span>{review.limitations}</span>
+            <div className='review__conteiner-limitation limitation'>
+                <span className='limitation-title'>Недостатки</span>
+                <span className='limitation-text'>{review.limitations}</span>
             </div>
-            <span className='review__conteiner-comment'>Комментарий</span>
-            <span className='review__conteiner-commentText'>{review.comment}</span>
-            <div className='review__conteiner-appraisal'>
+            <span className='review__conteiner-title'>Комментарий</span>
+            <span className='review__conteiner-text'>{review.comment}</span>
+            <div className='review__conteiner-appraisal appraisal'>
                 <span></span>
-                <span>Советует</span>
+                <span className='appraisal-text'>Советует</span>
             </div>
-            <div className='review__conteiner-time'>
-                <span>1 минуту назад</span>
-                <span>Ответить</span>
+            <div className='review__conteiner-answer answer'>
+                <span className='answer-time'>1 минуту назад</span>
+                <span className='answer-text'>Ответить</span>
             </div>
         </div>
     )
