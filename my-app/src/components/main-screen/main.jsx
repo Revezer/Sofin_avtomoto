df8fdc24e9af060e3eb534d6eac8aabe15170106
Revezer <64742445+Revezer@users.noreply.github.com>
2021-07-19 19:59:12 +0300

    отзывы

--- my-app/src/components/main-screen/main.jsx
+++ my-app/src/components/main-screen/main.jsx
@@ -14,6 +14,7 @@ import SpecificationsComponent from './specifications'
 import {connect} from 'react-redux'
 import {slideSelection} from '../../store/action'
 import ReviewsComponent from './reviews'
+import FooterComponent from './footer'
 
 const Main = (props) => {
     const {slideNumber, selectSlide} = props
@@ -84,6 +85,7 @@ const Main = (props) => {
                 {/* <SpecificationsComponent /> */}
                 <ReviewsComponent />
             </main>
+            <FooterComponent />
         </div>
     )
 }
