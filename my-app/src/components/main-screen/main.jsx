c9093f18674977c492c8ff768b4e798434d4baad
Revezer <64742445+Revezer@users.noreply.github.com>
2021-07-27 16:35:27 +0300

    исправления тестов

--- my-app/src/components/main-screen/main.jsx
+++ my-app/src/components/main-screen/main.jsx
@@ -75,7 +75,7 @@ const Main = (props) => {
     return (
         <>
             <HeaderComponent />
-            <main className='main'>
+            <main inert className='main'>
                 <div className='main__conteiner conteiner'>
                     <div>
                         <div className='conteiner__photo'>
--- my-app/src/components/main-screen/popup.jsx
+++ my-app/src/components/main-screen/popup.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {connect} from 'react-redux'
 import {addReview, openPopup} from '../../store/action'
 import PropTypes from 'prop-types'
+import FocusTrap from 'focus-trap-react'
 
 const Popup = (props) => {
     const {reviews, addNewReview, closePopup} = props
@@ -44,41 +45,43 @@ const Popup = (props) => {
     const fiveStar = newReview.appraisal > 4 ? 'popup__redstar popup__star-focus' : 'popup__star popup__star-focus'
 
     return (
-        <form className='review__popup' onSubmit={handleSubmit}>
-            <span className='popup__name'>Оставить отзыв</span>
-            <div className='popup__block'>
-                <div className='popup__conteiner'>
-                    <input autoFocus={true} className='popup__text' placeholder='Имя' required name='name' onChange={handleFieldChange}></input>
-                    <input className='popup__text' placeholder='Достоинства' name='dignity' onChange={handleFieldChange}></input>
-                    <input className='popup__text' placeholder='Недостатки' name='limitations' onChange={handleFieldChange}></input>
-                </div>
-                <div className='popup__conteiner'>
-                    <div className='popup__block-rating'>
-                        <span className='popup__title'>Оцените товар:</span>
-                        <div className='popup__rating' onClick={handleFieldChange}>
-                            <label className={oneStar}>
-                                <input className='visually-hidden' name='appraisal' defaultValue={1} type='radio'/>
-                            </label>
-                            <label className={twoStar}>
-                                <input className='visually-hidden' name='appraisal' defaultValue={2} type='radio'/>
-                            </label>
-                            <label className={threeStar}>
-                                <input className='visually-hidden' name='appraisal' defaultValue={3} type='radio'/>
-                            </label>
-                            <label className={fourStar}>
-                                <input className='visually-hidden' name='appraisal' defaultValue={4} type='radio'/>
-                            </label>
-                            <label className={fiveStar}>
-                                <input className='visually-hidden' name='appraisal' defaultValue={5} type='radio'/>
-                            </label>
-                        </div>
+        <FocusTrap>
+            <form className='review__popup' onSubmit={handleSubmit}>
+                <span className='popup__name'>Оставить отзыв</span>
+                <div className='popup__block'>
+                    <div className='popup__conteiner'>
+                        <input autoFocus={true} className='popup__text' placeholder='Имя' required name='name' onChange={handleFieldChange}></input>
+                        <input className='popup__text' placeholder='Достоинства' name='dignity' onChange={handleFieldChange}></input>
+                        <input className='popup__text' placeholder='Недостатки' name='limitations' onChange={handleFieldChange}></input>
                     </div>
-                    <textarea className='popup__text popup__text-comment' placeholder='Комментарий' required name='comment' onChange={handleFieldChange}></textarea>
+                    <div className='popup__conteiner'>
+                        <div className='popup__block-rating'>
+                            <span className='popup__title'>Оцените товар:</span>
+                            <div className='popup__rating' onClick={handleFieldChange}>
+                                <label className={oneStar}>
+                                    <input className='visually-hidden' name='appraisal' defaultValue={1} type='radio'/>
+                                </label>
+                                <label className={twoStar}>
+                                    <input className='visually-hidden' name='appraisal' defaultValue={2} type='radio'/>
+                                </label>
+                                <label className={threeStar}>
+                                    <input className='visually-hidden' name='appraisal' defaultValue={3} type='radio'/>
+                                </label>
+                                <label className={fourStar}>
+                                    <input className='visually-hidden' name='appraisal' defaultValue={4} type='radio'/>
+                                </label>
+                                <label className={fiveStar}>
+                                    <input className='visually-hidden' name='appraisal' defaultValue={5} type='radio'/>
+                                </label>
+                            </div>
+                        </div>
+                        <textarea className='popup__text popup__text-comment' placeholder='Комментарий' required name='comment' onChange={handleFieldChange}></textarea>
+                    </div>
                 </div>
-            </div>
-            <button className='popup__button' type='submit'>оставить отзыв</button>
-            <button className='popup__button-close' onClick={() => handleClosePopup()}></button>
-        </form>
+                <button className='popup__button' type='submit'>оставить отзыв</button>
+                <button className='popup__button-close' onClick={() => handleClosePopup()}></button>
+            </form>
+        </FocusTrap>
     )
 }
 
